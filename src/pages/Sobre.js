3b9aaa831122f4e01266ce0eebd7457d8cb89be0
lucas ferreira <lucas.simoes_iteris@prestador.globo>
2021-12-21 11:00:51 -0300

    animações

--- src/pages/Sobre.js
+++ src/pages/Sobre.js
@@ -1,5 +1,4 @@
-
-import React, { useState } from 'react'
+import React, { createRef } from 'react'
 import CardInfo from '../components/CardInfo';
 import "../styles/pages/sobre.css"
 
@@ -10,14 +9,19 @@ function Sobre() {
     let hr = document.getElementById("hr1");
     let button = document.getElementById("button-history")
     if (sec.style.display === "none") {
+      sec.classList.remove("splash-out-animation");
+      sec.classList.add("splash-in-animation")
       sec.style.display = "flex";
       hr.style.display = "none";
       button.classList.remove("down");
       button.classList.add("up");
-
     } else {
-      sec.style.display = "none";
-      hr.style.display = "block";
+      sec.classList.remove("splash-in-animation");
+      sec.classList.add("splash-out-animation");
+      setTimeout(() => {
+        sec.style.display = "none";
+        hr.style.display = "block";
+      }, 590)
       button.classList.remove("up");
       button.classList.add("down");
     }
@@ -26,28 +30,46 @@ function Sobre() {
   function handleShowTextGraduation() {
     let sec = document.getElementById("graduate");
     let hr = document.getElementById("hr2");
+    let button = document.getElementById("button-graduation");
     if (sec.style.display === "none") {
+      sec.classList.remove("splash-out-animation");
+      sec.classList.add("splash-in-animation")
       sec.style.display = "flex";
       hr.style.display = "none";
-
+      button.classList.remove("down");
+      button.classList.add("up");
     } else {
-      sec.style.display = "none";
-      hr.style.display = "block";
-
+      sec.classList.remove("splash-in-animation");
+      sec.classList.add("splash-out-animation");
+      setTimeout(() => {
+        sec.style.display = "none";
+        hr.style.display = "block";
+      }, 590)
+      button.classList.remove("up");
+      button.classList.add("down");
     }
   }
 
   function handleShowTextHobbies() {
     let sec = document.getElementById("hobbies");
     let hr = document.getElementById("hr3");
+    let button = document.getElementById("button-hobbies");
     if (sec.style.display === "none") {
+      sec.classList.remove("splash-out-animation");
+      sec.classList.add("splash-in-animation")
       sec.style.display = "flex";
       hr.style.display = "none";
-
+      button.classList.remove("down");
+      button.classList.add("up");
     } else {
-      sec.style.display = "none";
-      hr.style.display = "block";
-
+      sec.classList.remove("splash-in-animation");
+      sec.classList.add("splash-out-animation");
+      setTimeout(() => {
+        sec.style.display = "none";
+        hr.style.display = "block";
+      }, 590)
+      button.classList.remove("up");
+      button.classList.add("down");
     }
 
   }
@@ -60,20 +82,20 @@ function Sobre() {
         </span>
       </h2>
       <div className="horizontal-line" id="hr1" />
-      <section id="history">
+      <section className="card-container" id="history">
         <CardInfo
           illustra="https://images.unsplash.com/photo-1551736947-ca1f44a894e2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1389&q=80"
           textInfo="Sou nascido em São Paulo capital, porém me considero gaúcho. Criado em Porto Alegre, já passei um tempo em Taquara e já fazem mais de 10 anos que moro em Pelotas.
           Tenho 21 anos e desde pequeno gostei de video-games, computadores e tecnologia."
         />
       </section>
-      {/* <h2 className="header">Formação
+      <h2 className="header">Formação
         <span>
-          <button className={toggleButtonGraduate} onClick={handleShowTextGraduation}></button>
+          <button className="arrow" id="button-graduation" onClick={handleShowTextGraduation}></button>
         </span>
       </h2>
       <div className="horizontal-line" id="hr2" />
-      <section id="graduate">
+      <section className="card-container" id="graduate">
         <CardInfo
           illustra="https://images.unsplash.com/photo-1525921429624-479b6a26d84d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
           textInfo="Passei por uma experiência escolar na Inglaterra em 2015, fazendo 2 meses de aula ao equivalente primeiro ano do ensino médio aqui no Brasil.
@@ -83,16 +105,16 @@ function Sobre() {
       </section>
       <h2 className="header">Hobbies
         <span>
-          <button className={toggleButtonHobbies} onClick={handleShowTextHobbies}></button>
+          <button className="arrow" id="button-hobbies" onClick={handleShowTextHobbies}></button>
         </span>
       </h2>
       <div className="horizontal-line" id="hr3" />
-      <section className="text" id="hobbies">
+      <section className="card-container" id="hobbies">
         <CardInfo
           illustra="https://images.unsplash.com/photo-1542652694-40abf526446e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
           textInfo="Gosto de acompanhar muitos esportes, principalmente basquete e fórmula 1. Além disso eu faço alguns desenhos quando tenho vontade, alguns deles coloquei no meu instagram. Além de estressar um pouco jogando CS ou Valorant."
         />
-      </section> */}
+      </section>
     </div>
   )
 
